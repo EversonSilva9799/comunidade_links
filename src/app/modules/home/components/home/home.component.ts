fef2bc56ea19b3cdff454bc5ee3203ccfeb74122
Everson Silva <eversonsilva9799@gmail.com>
2020-05-12 07:19:45 -0300

    Pegar posts a partir do postService do db e titulo

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Post from 'src/app/data/models/Post';
 import { PostService } from 'src/app/data/services/post.service';
+import Response from 'src/app/data/models/Response';
+import { Title } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-home',
@@ -10,13 +12,18 @@ import { PostService } from 'src/app/data/services/post.service';
 export class HomeComponent implements OnInit {
 	public posts: Post[];
 
-	constructor(private postService: PostService) {}
+  constructor(private postService: PostService, private titleService: Title) {
+    this.titleService.setTitle(`Comunidade Links`);
+  }
 
 	ngOnInit(): void {
-		this.getLinks();
-	}
+    this.getPosts();
+  }
 
-	public getLinks() {
-		this.posts = this.postService.getLinks();
-	}
+  public getPosts() {
+    this.postService.index().subscribe((response: Response) => {
+      this.posts = response.data.docs;
+    });
+  }
+
 }
