1b2bec4d9ca8d1e564b96150a73f5028d35dd423
Everson Silva <eversonsilva9799@gmail.com>
2020-05-12 07:23:00 -0300

    Componente de pesquisa de post

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Post  from '../../../data/models/Post';
+import { SearchService } from 'src/app/data/services/search.service';
+import Response from 'src/app/data/models/Response';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public posts: Post[] = [];
+  public word: string;
+
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
+  }
+
+  public search() {
+    if (this.word.length > 0) {
+      this.searchService.get(this.word).subscribe((response: Response) => {
+        this.posts = response.data.docs;
+      });
+    }
+    else {
+      this.posts = [];
+    }
+  }
+
+  public clearSearch() {
+    if (this.word.length < 0) {
+      this.posts = [];
+    }
+
   }
 
 }
