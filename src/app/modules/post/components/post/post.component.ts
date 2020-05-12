d834ff1a1122d2c5cbfe0912dfdbd2f88049e897
Everson Silva <eversonsilva9799@gmail.com>
2020-05-12 07:20:50 -0300

    Criar método para redirecionar para curso clicado

--- src/app/modules/post/components/post/post.component.ts
+++ src/app/modules/post/components/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import Post from 'src/app/data/models/Post';
+import { ApiService } from 'src/app/data/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-post',
@@ -9,7 +11,11 @@ import Post from 'src/app/data/models/Post';
 export class PostComponent implements OnInit {
 	@Input() post: Post;
 
-	constructor() {}
+	constructor(public apiService: ApiService, private router: Router) {}
 
-	ngOnInit(): void {}
+  ngOnInit(): void {}
+
+  public redirect(link: string) {
+    this.router.navigate([`/posts${link}`]);
+  }
 }
