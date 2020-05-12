b10f10a43bb087584e2aa58838eca40e121438b1
Everson Silva <eversonsilva9799@gmail.com>
2020-05-12 07:22:04 -0300

    Pegar post a partir do db e setar title da página

--- src/app/modules/post/components/posts-detail/posts-detail.component.ts
+++ src/app/modules/post/components/posts-detail/posts-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import Post from 'src/app/data/models/Post';
 import { PostService } from 'src/app/data/services/post.service';
+import Response from 'src/app/data/models/Response';
+import { ApiService } from 'src/app/data/services/api.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-posts-detail',
@@ -9,17 +12,27 @@ import { PostService } from 'src/app/data/services/post.service';
 	styleUrls: [ './posts-detail.component.css' ]
 })
 export class PostsDetailComponent implements OnInit {
-	public post: Post;
+	public post: Post = new Post();
 	public link: string;
 
-	constructor(private route: ActivatedRoute, private postService: PostService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    public apiService: ApiService,
+    private titleService: Title) {}
 
 	ngOnInit(): void {
-		this.route.params.subscribe(({ link }) => (this.link = link));
-		this.getPost();
+		this.route.params.subscribe(({ link }) =>{
+      this.link = link;
+      this.get();
+    });
+
 	}
 
-	public getPost(): void {
-		this.post = this.postService.getPost();
+	public get(): void {
+		this.postService.get(this.link).subscribe((response: Response) => {
+      this.post = response.data;
+      this.titleService.setTitle(`${this.post.title} | Comunidade Links`);
+    });
 	}
 }
