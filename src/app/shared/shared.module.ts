7b4bbb56e57754dd97c7a0fe000d6e61a7748d9a
Everson Silva <eversonsilva9799@gmail.com>
2020-05-12 07:22:34 -0300

    Importar forms module e router module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SearchComponent } from './components/search/search.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
 	declarations: [ SearchComponent, BannerComponent, NotFoundComponent ],
-	imports: [ CommonModule ],
+  imports: [CommonModule, FormsModule, RouterModule ],
 	exports: [ SearchComponent, BannerComponent, NotFoundComponent ]
 })
 export class SharedModule {}
