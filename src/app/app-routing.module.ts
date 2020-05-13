e8800909c5d40428b5e11e57b7a458c8621b72e2
Everson Silva <eversonsilva9799@gmail.com>
2020-05-13 08:08:38 -0300

    Adicionar configurações de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { LayoutComponent } from './layout/components/layout/layout.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
@@ -16,14 +16,22 @@ const routes: Routes = [
 			{
 				path: 'posts',
 				loadChildren: './modules/post/post.module#PostModule'
-			}
-		]
-	},
+      },
+		],
+  },
+
 	{ path: '**', component: NotFoundComponent }
 ];
 
+const routerOptions: ExtraOptions = {
+  useHash: true,
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
+
 @NgModule({
-	imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
+  imports: [RouterModule.forRoot(routes, routerOptions) ],
 	exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
