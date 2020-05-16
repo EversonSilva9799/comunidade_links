8ee17ad58b0f05ae942e2c8ab7f95c34d8fab794
Everson Silva <eversonsilva9799@gmail.com>
2020-05-16 18:18:27 -0300

    Importar dashboard module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LinkModule } from '../modules/link/link.module';
 import { HomeModule } from '../modules/home/home.module';
 import { PostModule } from '../modules/post/post.module';
+import { DashboardModule } from '../modules/dashboard/dashboard.module';
 
 @NgModule({
 	declarations: [],
-	imports: [ CommonModule, HomeModule, LinkModule, PostModule ]
+	imports: [ CommonModule, HomeModule, LinkModule, PostModule, DashboardModule]
 })
 export class CoreModule {}
