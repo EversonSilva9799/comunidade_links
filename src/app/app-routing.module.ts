e4e115e6a0bf7365145b781b0112530293334d85
Everson Silva <eversonsilva9799@gmail.com>
2020-05-16 18:15:27 -0300

    Editar modo que é importado os mudulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,33 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { LayoutComponent } from './layout/components/layout/layout.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { LayoutDashboardComponent } from './layout/components/layout-dashboard/layout-dashboard.component';
+import { HomeModule } from './modules/home/home.module';
+import { PostModule } from './modules/post/post.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: LayoutDashboardComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => DashboardModule
+      }
+    ]
+  },
 	{
 		path: '',
 		component: LayoutComponent,
-
 		children: [
 			{
 				path: '',
-				loadChildren: './modules/home/home.module#HomeModule'
+				loadChildren: () => HomeModule
 			},
 			{
 				path: 'posts',
-				loadChildren: './modules/post/post.module#PostModule'
+				loadChildren: () => PostModule
       },
 		],
   },
