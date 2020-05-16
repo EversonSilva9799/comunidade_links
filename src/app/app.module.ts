7421cfb43a797d9a5264bc4fe90ae3078952874e
Everson Silva <eversonsilva9799@gmail.com>
2020-05-16 18:17:44 -0300

    Adicionar toatr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { CoreModule } from "./core/core.module";
-import { LayoutModule } from "./layout/layout.module";
+import { ToastrModule } from "ngx-toastr";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    CoreModule,
+    LayoutModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
